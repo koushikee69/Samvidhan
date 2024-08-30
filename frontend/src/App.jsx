bcf4e2946a1733daa555ce6ab933de87e445ca58
Tanmay Sahu <sahutanmay1903@gmail.com>
2024-08-30 14:14:33 +0530

    initial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import Login from "./components/Login";
+import Level from "./components/Level";
+import levels from "./json/levels.json";
+import Home from "./components/Home";
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
+  
+  const lvl = 1;
   return (
-    <div>
-      <Login />
-    </div>
+    // <div>
+    //   {/* <Login /> */}
+    //   {/* {levels.levels.map((data, index) => (
+    //    lvl===data.level && <Level key={index} data={data} />
+    //   ))} */}
+    //   <Home />
+    // </div>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/levels" element={<Level />} /> {/* Add your routes here */}
+  </Routes>
   );
 };
 
